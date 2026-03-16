78d6b44a5994d4c1c519fa404b8425de3a43c4e9
surafel-22 <salembirhanu3@gmail.com>
2026-03-16 09:50:10 +0300

    day 32 final

--- Day 30-39/day32/main.py
+++ Day 30-39/day32/main.py
@@ -22,6 +22,10 @@
 from datetime import datetime
 import pandas
 import random
+import smtplib
+
+MY_EMAIL = "ASD@gmail.com"
+PASSWORD = "asdfg"
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -30,8 +34,15 @@ data = pandas.read_csv("birthdays.csv")
 birthday_dict = {(data_row.month,data_row.day):data_row for (index, data_row) in data.iterrows()}
 
 if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
     file_path = f"letter_{random.randint(1,3)}"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents = contents.replace("[Name]", " ")
+        contents = contents.replace("[Name]", birthday_person["name"] )
+    with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(MY_EMAIL,birthday_person["email"],msg=f"Subject:Happy Birthday!\n\n{contents}")
+
+
 
