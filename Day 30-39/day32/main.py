c291aef341fc27e2432910c6d68eed41d351d5ba
surafel-22 <salembirhanu3@gmail.com>
2026-03-16 09:30:39 +0300

    birthday project update

--- Day 30-39/day32/main.py
+++ Day 30-39/day32/main.py
@@ -1,43 +1,37 @@
-# # import smtplib
-# #
-# # my_email = "salembirhanu3@gmail.com"
-# # password = "19"
-# #
-# # with smtplib.SMTP('smtp.gmail.com') as connection:
-# #     connection.starttls()
-# #     connection.login(user=my_email, password=password)
-# #     connection.sendmail(from_addr=my_email,
-# #                         to_addrs="surebahiru22@gmail.com",
-# #                         msg="Subject:hello\n\n this is the body")
-# #     connection.close()
-# 
-# 
 # import datetime as dt
+# import smtplib
+# from random import *
+# 
+# MY_EMAIL = "HHH@gmail.com"
+# PASSWORD = "SL;J"
 # 
 # now = dt.datetime.now()
-# print(now.year)
+# day_of_week = now.weekday()
 # 
-# date_of_birth = dt.datetime(year=2005 ,month=9, day=29 ,hour=17)
-# print(date_of_birth)
+# if day_of_week == 0:
+#     with open("quotes.txt", "r") as f:
+#         all_quotes = f.readlines()
+#         qoute = choice(all_quotes)
+#     print(qoute)
+#     connection =smtplib.SMTP("smtp.gmail.com")
+#     connection.starttls()
+#     connection.login(user=MY_EMAIL, password=PASSWORD)
+#     connection.sendmail(from_addr=MY_EMAIL,to_addrs=MY_EMAIL,msg=f"Subject:Monday Motivation\n\n {qoute}")
+#     connection.close()
+
+from datetime import datetime
 import pandas
-import pandas as pd
-import datetime as dt
-import smtplib
-from random import *
+import random
 
-MY_EMAIL = "HHH@gmail.com"
-PASSWORD = "SL;J"
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row.month,data_row.day):data_row for (index, data_row) in data.iterrows()}
 
-if day_of_week == 0:
-    with open("quotes.txt", "r") as f:
-        all_quotes = f.readlines()
-        qoute = choice(all_quotes)
-    print(qoute)
-    connection =smtplib.SMTP("smtp.gmail.com")
-    connection.starttls()
-    connection.login(user=MY_EMAIL, password=PASSWORD)
-    connection.sendmail(from_addr=MY_EMAIL,to_addrs=MY_EMAIL,msg=f"Subject:Monday Motivation\n\n {qoute}")
-    connection.close()
+if today_tuple in birthday_dict:
+    file_path = f"letter_{random.randint(1,3)}"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[Name]", " ")
+
