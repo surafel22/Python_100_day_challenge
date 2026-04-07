2e68bc82b52b9e6e5a84652e6d21e40797c6d4a3
surafel-22 <salembirhanu3@gmail.com>
2026-04-07 09:37:51 +0300

    day 34 @ all completed

--- Day 30-39/day34/quizzer/main.py
+++ Day 30-39/day34/quizzer/main.py
@@ -12,7 +12,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-gui = QuizInterface()
+gui = QuizInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
--- Day 30-39/day34/quizzer/ui.py
+++ Day 30-39/day34/quizzer/ui.py
@@ -3,17 +3,19 @@ from quiz_brain import *
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR,padx=20,pady=20)
 
-        correct_image = PhotoImage(file="./images/true.png")
-        self.correct = Button(image=correct_image)
+        correct_image = PhotoImage(file="images/true.png")
+        self.correct = Button(image=correct_image, command=self.true_pressed)
         self.correct.grid(row=2,column=0)
 
-        wrong_image = PhotoImage(file="./images/false.png")
-        self.wrong = Button(image=wrong_image)
+        wrong_image = PhotoImage(file="images/false.png")
+        self.wrong = Button(image=wrong_image, command=self.false_pressed)
         self.wrong.grid(row=2,column=1)
 
         self.score_label = Label(text="Score:", fg="white", bg=THEME_COLOR)
@@ -22,6 +24,7 @@ class QuizInterface:
         self.canvas = Canvas(self.window,width=300,height=250,bg=THEME_COLOR)
         self.question_text = self.canvas.create_text(150,
                                                      125,
+                                                     width = 280,
                                                      text="some questions" ,
                                                      fill = THEME_COLOR,
                                                      font=("Arial",20,"italics")
@@ -29,6 +32,32 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2,pady=50)
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(background="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score} points")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text,text="You have reached the end of the quiz")
+            self.correct.config(state="disabled")
+            self.wrong.config(state="disabled")
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.get_feedback(is_right)
+
+    def false_pressed(self):
+        self.get_feedback(self.quiz.check_answer("False"))
+
+    def get_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000,self.get_next_question)
 
 
 
